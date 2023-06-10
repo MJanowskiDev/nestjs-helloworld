8e98ac030a7359ef99f02a39ec9f840e2d561e33
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-10 11:30:26 +0200

    feat: implemented first middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { ArmyModule } from './army/army.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [ArmyModule],
   controllers: [AppController, AdminController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '/army', method: RequestMethod.GET });
+  }
+}
