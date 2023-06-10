baa392d97192043a72a2d47775807ef34c1838b0
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-10 11:18:09 +0200

    Feat added module exports and global flag

--- src/army/army.module.ts
+++ src/army/army.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ArmyController } from './army.controller';
 import { ArmyService } from './army.service';
+
+@Global() //global modules can be registered only once
 @Module({
   controllers: [ArmyController],
   providers: [ArmyService],
+  exports: [ArmyService],
 })
 export class ArmyModule {}
