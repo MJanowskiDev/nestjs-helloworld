40f518ff4cb8ba3b939e1b5df5e597e2173b9db6
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-11 20:59:50 +0200

    feat: custom exception

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from './common/exceptions/forbidden.exception';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    throw new ForbiddenException();
   }
 }
