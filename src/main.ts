e7b0af1b01f691ed1024298efbc400603f64c7de
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-12 20:04:37 +0200

    feat:implemented global http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FunctionalLoggerMiddleware } from './common/middleware/functional-logger/functional-logger.middleware';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(FunctionalLoggerMiddleware);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
