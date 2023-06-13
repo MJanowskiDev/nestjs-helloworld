178da0ad19a691cebd581f40a7a03c29381e6fb1
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-06-13 20:34:54 +0200

    Implemented transform interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,19 @@ import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { ArmyModule } from './army/army.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 @Module({
   imports: [ArmyModule],
   controllers: [AppController, AdminController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
